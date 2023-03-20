7a479038ec99096cda1cd2df5589c27049b88fd4
zzzkan <zzz602214076@outlook.com>
2023-03-20 23:49:22 +0900

    Update the color theme

---
+++ src/@zzzkan/gatsby-theme-blog/theme/colors.ts
@@ -0,0 +1,16 @@
+const colors = {
+  tint: { default: "red.700", _dark: "red.200" },
+  primaryText: { default: "gray.800", _dark: "gray.100" },
+  secondaryText: { default: "gray.600", _dark: "gray.400" },
+  onTintText: { default: "white", _dark: "gray.900" },
+  primaryBackground: { default: "white", _dark: "gray.900" },
+  secondaryBackground: {
+    default: "RGBA(0, 0, 0, 0.02)",
+    _dark: "RGBA(255, 255, 255, 0.02)",
+  },
+  highlightCode: {
+    default: "RGBA(0, 255, 255, 0.05)",
+  },
+}
+
+export default colors
